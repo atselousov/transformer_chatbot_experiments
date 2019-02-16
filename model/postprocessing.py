2459b7dbb05a4739377331aeab922259c6a22dee
atselousov <aleksander.tselousov@yandex.ru>
2019-02-16 22:31:08 +0300

    synonym augmentation.

--- model/postprocessing.py
+++ model/postprocessing.py
@@ -26,6 +26,7 @@ from .retrieval import RetrievalBot
 import re
 import numpy as np
 from itertools import combinations
+from collections import defaultdict
 
 
 SPELL_EXCEPTIONS = ['lol']
@@ -242,6 +243,36 @@ def get_syn(seq):
 
     key = random.choice(list(synonyms.keys()))
     return synonyms[key], key
+
+
+def augment_replica(seq):
+    pos2wn = {'NN': wordnet.NOUN,
+              'JJ': wordnet.ADJ,
+              'VBP': wordnet.VERB,
+              'RB': wordnet.ADV}
+
+    synonyms = defaultdict(list)
+
+    tagged_seq = seq.replace('i ', 'I ')
+    tagged_seq = nltk.pos_tag(nltk.word_tokenize(tagged_seq))
+
+    for word, pos in tagged_seq:
+        if pos not in pos2wn:
+            continue
+
+        pos = pos2wn[pos]
+        synnets = wordnet.synsets(word, pos=pos)
+
+        for synnet in synnets:
+            for syn in synnet.lemma_names():
+                if syn != word:
+                    synonyms[word].append(syn)
+            break
+    if synonyms:
+        for key, values in synonyms.items():
+            seq = seq.replace(key, random.choice(list(values)))
+
+    return seq
 
 
 def equal_phrases(phrases):
