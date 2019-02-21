8709b20ad08b627a7e24f6afc596aa231bb04054
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-02-21 08:58:48 +0300

    common dockerfile for train and inference

--- neuro/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-neuro job submit -x --http 8888 --ssh 22 --memory 8G --cpu 4 --gpu 1 \
-                    --gpu-model nvidia-tesla-v100 \
-                    --volume storage://rauf-kurbanov/transformer_chatbot/parameters:/workspace/parameterst:ro \
-                    --volume storage://rauf-kurbanov/transformer_chatbot/checkpoints:/workspace/checkpoints:ro \
-                    raufkurbanov/transformer_chatbot:latest \
