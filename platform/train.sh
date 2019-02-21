9e427503ac72f24120bda613517181926d10cf71
kyryl <truskovskiyk@gmail.com>
2019-02-21 18:41:21 +0200

    update

--- platform/train.sh
+++ platform/train.sh
@@ -12,20 +12,6 @@ CMD="python train.py"
 DESCRIPTION="experiment#1"
 CONFIGURATION="./platform/configurations/experiment1"
 
-#neuro job submit \
-#        --cpu ${CPU} --gpu ${GPU} --memory ${MEM} --gpu-model nvidia-tesla-v100 \
-#        --http 8080 --ssh 22  \
-#        --volume ${DATA_ROOT}/meteor-1.5/:/workspace/meteor-1.5:rw \
-#        --volume ${DATA_ROOT}/datasets/:/workspace/datasets:rw \
-#        --volume ${DATA_ROOT}/parameters:/workspace/parameters:rw \
-#        --volume ${DATA_ROOT}/runs/:/workspace/runs:rw \
-#        --non-preemptible \
-#        --description ${DESCRIPTION} \
-#        --env-file ${CONFIGURATION} \
-#        ${IMAGE} \
-#        "${CMD}"
-
-
 neuro job submit \
         --cpu ${CPU} --gpu ${GPU} --memory ${MEM} --gpu-model nvidia-tesla-v100 \
         --http 8080 --ssh 22  \
@@ -36,6 +22,20 @@ neuro job submit \
         --non-preemptible \
         --description ${DESCRIPTION} \
         --env-file ${CONFIGURATION} \
-        ${IMAGE}
+        ${IMAGE} \
+        "${CMD}"
+
+#
+#neuro job submit \
+#        --cpu ${CPU} --gpu ${GPU} --memory ${MEM} --gpu-model nvidia-tesla-v100 \
+#        --http 8080 --ssh 22  \
+#        --volume ${DATA_ROOT}/meteor-1.5/:/workspace/meteor-1.5:rw \
+#        --volume ${DATA_ROOT}/datasets/:/workspace/datasets:rw \
+#        --volume ${DATA_ROOT}/parameters:/workspace/parameters:rw \
+#        --volume ${DATA_ROOT}/runs/:/workspace/runs:rw \
+#        --non-preemptible \
+#        --description ${DESCRIPTION} \
+#        --env-file ${CONFIGURATION} \
+#        ${IMAGE}
 #neuro job submit --non-preemptible -c 2 -m 4GB --http 8080 -v storage://truskovskiyk/convai/runs:/data/ tensorflow/tensorflow "tensorboard --logdir /data --port 8080"
 #neuro model debug --localport 12789
