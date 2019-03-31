5c735d7881aa9f48fee5bd8d8cfe89e8ba457171
atselousov <aleksander.tselousov@yandex.ru>
2019-03-31 15:58:05 +0300

    left padding

--- model/utils.py
+++ model/utils.py
@@ -53,13 +53,12 @@ def repeat_along_dim1(obj, repetitions):
     return obj.repeat([repetitions] + [1] * len(obj.shape[1:]))
 
 
-def pad_sequence(sequences, batch_first=False, padding_value=0):
+def pad_sequence(sequences, batch_first=False, padding_value=0, left=False):
     # assuming trailing dimensions and type of all the Tensors
     # in sequences are same and fetching those from sequences[0]
     if not len(sequences):
         return torch.empty(0)
-    max_size = sequences[0].size()
-    trailing_dims = max_size[1:]
+    trailing_dims = sequences[0].size()[1:]
     max_len = max([s.size(0) for s in sequences])
     if batch_first:
         out_dims = (len(sequences), max_len) + trailing_dims
@@ -70,10 +69,16 @@ def pad_sequence(sequences, batch_first=False, padding_value=0):
     for i, tensor in enumerate(sequences):
         length = tensor.size(0)
         # use index notation to prevent duplicate references to the tensor
-        if batch_first:
-            out_tensor[i, :length, ...] = tensor
+        if left:
+            if batch_first:
+                out_tensor[i, -length:] = tensor
+            else:
+                out_tensor[-length:, i] = tensor
         else:
-            out_tensor[:length, i, ...] = tensor
+            if batch_first:
+                out_tensor[i, :length, ...] = tensor
+            else:
+                out_tensor[:length, i, ...] = tensor
 
     return out_tensor
 
