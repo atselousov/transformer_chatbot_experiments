f3def8150da14ad1978244974415f3dc9400ab46
Sergey Golovanov <Sergey_XG@mail.ru>
2019-04-05 14:17:03 +0300

    add new vocab

--- model/text.py
+++ model/text.py
@@ -14,159 +14,180 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import spacy
+import json
+import re
+from collections import defaultdict, namedtuple
+from enum import Enum
+
 import ftfy
-from collections import Counter, defaultdict
+import spacy
 from tqdm import trange
 
-class SpacyLowerTokenizer:
-    def __init__(self):
-        self.tokenizer = spacy.load('en', disable=['parser', 'tagger', 'ner', 'textcat'])
 
-    def __call__(self, string):
-        string = ftfy.fix_text(string)
-        words = [t.text.strip() for t in self.tokenizer(string)]
-        words = [w.lower() for w in words if w]
-
-        return words
-
-class BPEVocab:
-    we = '</w>'
-
-    pad_token = '<pad>'
-    bos_token = '<s>'
-    eos_token = '</s>'
-    info_bos = '<i>'
-    info_eos = '</i>'
-    talker1_bos = '<t1>'
-    talker1_eos = '</t1>'
-    talker2_bos = '<t2>'
-    talker2_eos = '</t2>'
-    sent_dialog_token = '<s>'
-    info_dialog_token = '<i>'
-    talker1_dialog_token = '<t1>'
-    talker2_dialog_token = '<t2>'
+class GPTTokenizer:
+    word_end = '</w>'
 
     @staticmethod
-    def from_files(vocab_path, codes_path, *args, **kwargs):
+    def _text_standardize(text):
+        """
+        fixes some issues the spacy tokenizer had on books corpus
+        also does some whitespace standardization
+        """
+
+        text = text.replace('—', '-')
+        text = text.replace('–', '-')
+        text = text.replace('―', '-')
+        text = text.replace('…', '...')
+        text = text.replace('´', "'")
+        text = re.sub('''(-+|~+|!+|"+|;+|\?+|\++|,+|\)+|\(+|\\+|\/+|\*+|\[+|\]+|}+|{+|\|+|_+)''', r' \1 ', text)
+        text = re.sub('\s*\n\s*', ' \n ', text)
+        text = re.sub('[^\S\n]+', ' ', text)
+        text = text.strip()
+
+        return text
+
+    def __init__(self):
+        self.spacy_tokenizer = spacy.load('en', disable=['parser', 'tagger', 'ner', 'textcat'])
+
+    def tokenize(self, text):
+        text = ftfy.fix_text(text)
+        text = GPTTokenizer._text_standardize(text)
+        tokens = [t.text.lower() for t in self.spacy_tokenizer(text)]
+        tokens = [tuple(token[:-1]) + (token[-1] + GPTTokenizer.word_end,) for token in tokens]
+
+        return tokens
+
+    def detokenize(self, bpe_tokens):
+        return ''.join(bpe_tokens).replace(GPTTokenizer.word_end, ' ')
+
+
+class GPT2Tokenizer:
+    @staticmethod
+    def _bytes_to_unicode():
+        """
+        Returns list of utf-8 byte and a corresponding list of unicode strings.
+        The reversible bpe codes work on unicode strings.
+        This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+        When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+        This is a signficant percentage of your normal, say, 32K bpe vocab.
+        To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+        And avoids mapping to whitespace/control characters the bpe code barfs on.
+        """
+
+        bs = list(range(ord("!"), ord("~") + 1)) + \
+             list(range(ord("¡"), ord("¬") + 1)) + \
+             list(range(ord("®"), ord("ÿ") + 1))
+        cs = bs[:]
+
+        n = 0
+        for b in range(2 ** 8):
+            if b not in bs:
+                bs.append(b)
+                cs.append(2 ** 8 + n)
+                n += 1
+                
+        cs = [chr(n) for n in cs]
+        mapping = dict(zip(bs, cs))
+
+        return mapping
+
+    def __init__(self):
+        self.byte_encoder = GPT2Tokenizer._bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+
+        # Should haved added re.IGNORECASE so BPE merges can happen for capitalized versions of contractions
+        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+
+    def tokenize(self, text):
+        tokens = []
+        tokens = [''.join([self.byte_encoder[b] for b in t.encode('utf-8')]) for t in re.findall(self.pat, text)]
+        tokens = [tuple(token) for token in tokens]
+
+        return tokens
+
+    def detokenize(self, bpe_tokens):
+        text = ''.join(bpe_tokens)
+        text = bytearray([self.byte_decoder[c] for c in text]).decode('utf-8', errors='replace')
+
+        return text
+
+
+SpecialTokensTuple = namedtuple('SpecTokensTuple', ['pad',
+                                                    'bos',
+                                                    'eos',
+                                                    'info_bos',
+                                                    'info_eos',
+                                                    'talker1_bos',
+                                                    'talker1_eos',
+                                                    'talker2_bos',
+                                                    'talker2_eos',
+                                                    'sent_dialog',
+                                                    'info_dialog',
+                                                    'talker1_dialog',
+                                                    'talker2_dialog'])
+
+
+class BPEVocab:
+    @staticmethod
+    def from_files(vocab_path, codes_path, tokenizer, special_tokens):
         with open(vocab_path, 'r', encoding='utf-8') as vocab_file:
-            vocab = [t.strip() for t in vocab_file.readlines()]
+            vocab = json.load(vocab_file)
 
         with open(codes_path, 'r', encoding='utf-8') as codes_file:
             codes = [c.strip() for c in codes_file.readlines()]
 
-            if codes[0].startswith('#version'):
+            if codes[0].startswith('#version:'):
                 codes = codes[1:]
+            if len(codes[-1]) == 0:
+                codes = codes[:-1]
 
             codes = [tuple(c.split()) for c in codes if c]
 
-        return BPEVocab(vocab, codes, *args, **kwargs)
+        return BPEVocab(vocab, codes, tokenizer, special_tokens)
 
     @staticmethod
-    def get_pairs(string):
-        if len(string) < 2:
+    def _get_pairs(sequence):
+        if len(sequence) < 2:
             return set()
 
-        return set(zip(string[:-1], string[1:]))
+        return set(zip(sequence[:-1], sequence[1:]))
 
-    def __init__(self, vocab, codes, tokenizer=SpacyLowerTokenizer(), zero_shot=False):
-        if zero_shot: # only one additional token: BPEVocab.pad_token = <pad>
-            self.spec_tokens = [BPEVocab.pad_token]
-            self.bos_token = '"</w>'
-            self.eos_token = '"</w>'
-            self.info_bos = '.</w>'
-            self.info_eos = '.</w>'
-            self.talker1_bos = '"</w>'
-            self.talker1_eos = '"</w>'
-            self.talker2_bos = '"</w>'
-            self.talker2_eos = '"</w>'
-            self.sent_dialog_token = '"</w>'
-            self.info_dialog_token = '.</w>'
-            self.talker1_dialog_token = '"</w>'
-            self.talker2_dialog_token = '"</w>'
-        else:
-            #TODO: add check for special tokens
-            self.spec_tokens = [BPEVocab.pad_token, BPEVocab.bos_token, BPEVocab.eos_token,
-                                BPEVocab.info_bos, BPEVocab.info_eos, BPEVocab.talker1_bos,
-                                BPEVocab.talker1_eos, BPEVocab.talker2_bos, BPEVocab.talker2_eos]
+    def __init__(self, vocab, codes, tokenizer, special_tokens):
+        assert isinstance(special_tokens, SpecialTokensTuple)
 
-        vocab = self.spec_tokens + vocab
+        filtered_special_tokens = [t for t in special_tokens if t not in vocab]
+        special_token2id = {t: i for i, t in enumerate(filtered_special_tokens)}
+        token2id = {t: i + len(filtered_special_tokens) for t, i in vocab.items()}
+        token2id.update(special_token2id)
 
-        self.token2id = {t: i for i, t in enumerate(vocab)}
-        self.id2token = {i: t for i, t in enumerate(vocab)}
+        self.special_tokens = special_tokens
+        self.n_new_tokens = len(filtered_special_tokens)
+        self.token2id = token2id
+        self.id2token = {i: t for t, i in self.token2id.items()}
         self.bpe_ranks = dict(zip(codes, range(len(codes))))
         self.tokenizer = tokenizer
         self.cache = {}
+
+        for token_name, token in special_tokens._asdict().items():
+            setattr(self, token_name + '_id', self.token2id[token])
 
     def __len__(self):
         return len(self.token2id)
 
     @property
     def n_special_tokens(self):
-        return len(self.spec_tokens)
+        return self.n_new_tokens
 
     @property
     def special_tokens_ids(self):
-        return [self.token2id[t] for t in self.spec_tokens]
+        return [self.token2id[t] for t in self.special_tokens]
 
-    @property
-    def pad_id(self):
-        return self.token2id[self.pad_token]
-
-    @property
-    def bos_id(self):
-        return self.token2id[self.bos_token]
-
-    @property
-    def eos_id(self):
-        return self.token2id[self.eos_token]
-
-    @property
-    def info_bos_id(self):
-        return self.token2id[self.info_bos]
-
-    @property
-    def info_eos_id(self):
-        return self.token2id[self.info_eos]
-
-    @property
-    def talker1_bos_id(self):
-        return self.token2id[self.talker1_bos]
-
-    @property
-    def talker1_eos_id(self):
-        return self.token2id[self.talker1_eos]
-
-    @property
-    def talker2_bos_id(self):
-        return self.token2id[self.talker2_bos]
-
-    @property
-    def talker2_eos_id(self):
-        return self.token2id[self.talker2_eos]
-
-    @property
-    def sent_dialog_id(self):
-        return self.token2id[self.sent_dialog_token]
-
-    @property
-    def info_dialog_id(self):
-        return self.token2id[self.info_dialog_token]
-
-    @property
-    def talker1_dialog_id(self):
-        return self.token2id[self.talker1_dialog_token]
-
-    @property
-    def talker2_dialog_id(self):
-        return self.token2id[self.talker2_dialog_token]
-
-    def get_prefix2words(self, convai_dict, smoothing_freq=5):
+    def get_prefix2words(self, words_dict, smoothing_freq=5):
         # map BPE-prefix => dict(full_words beginning with BPE-prefix, associated words_counts)
         prefix2words = defaultdict(dict)
-        for i in trange(len(convai_dict)):
-            word = convai_dict[i]
-            freq = convai_dict.freq[word] + smoothing_freq
+        for i in trange(len(words_dict)):
+            word = words_dict[i]
+            freq = words_dict.freq[word] + smoothing_freq
             prefix = self._bpe(word)[0]
             prefix2words[prefix].update(dict([(word, freq)]))
 
@@ -181,11 +202,11 @@ class BPEVocab:
         if token in self.cache:
             return self.cache[token]
 
-        word = tuple(token[:-1]) + (token[-1] + BPEVocab.we,)
-        pairs = BPEVocab.get_pairs(word)
+        word = token
+        pairs = BPEVocab._get_pairs(word)
 
         if not pairs:
-            return (token + BPEVocab.we,)
+            return word
 
         while True:
             bigram = min(pairs, key=lambda pair: self.bpe_ranks.get(pair, float('inf')))
@@ -216,33 +237,72 @@ class BPEVocab:
             if len(word) == 1:
                 break
             else:
-                pairs = BPEVocab.get_pairs(word)
+                pairs = BPEVocab._get_pairs(word)
 
         self.cache[token] = word
 
         return word
 
     def string2ids(self, string):
-        tokens = self.tokenizer(string)
+        tokens = self.tokenizer.tokenize(string)
         bpe_tokens = sum([self._bpe(t) for t in tokens], tuple())
         ids = [self.token2id[t] for t in bpe_tokens if t in self.token2id]
 
         return ids
 
-    @staticmethod
-    def to_ids_list(list_obj):
-        # Take care of inputs with dialog embeddings (list of pairs, we keep only the first item in the pairs) and single int inputs
-        if len(list_obj) == 0:
-            return []
-        if isinstance(list_obj, int):
-            return [list_obj]
-        if isinstance(list_obj[0], int):
-            return list_obj
-        assert isinstance(list_obj[0][0], int)
-        return list(item[0] for item in list_obj)
-
     def ids2string(self, ids):
-        ids = self.to_ids_list(ids)
         bpe_tokens = [self.id2token[id] for id in ids]
+        string = self.tokenizer.detokenize(bpe_tokens)
 
-        return ''.join(bpe_tokens).replace(BPEVocab.we, ' ')
+        return string
+
+
+SPECIAL_TOKENS = SpecialTokensTuple(pad='<pad>',
+                                    bos='<s>',
+                                    eos='</s>',
+                                    info_bos='<i>',
+                                    info_eos='</i>',
+                                    talker1_bos='<t1>',
+                                    talker1_eos='</t1>',
+                                    talker2_bos='<t2>',
+                                    talker2_eos='</t2>',
+                                    sent_dialog='<sent>',
+                                    info_dialog='<info>',
+                                    talker1_dialog='<talker1>',
+                                    talker2_dialog='<talker2>')
+
+
+ZERO_SHOT_SPECIAL_TOKENS = SpecialTokensTuple(pad='<pad>',
+                                              bos='^',
+                                              eos='_',
+                                              info_bos='<',
+                                              info_eos='>',
+                                              talker1_bos='{',
+                                              talker1_eos='}',
+                                              talker2_bos='[',
+                                              talker2_eos=']',
+                                              sent_dialog='|',
+                                              info_dialog='@',
+                                              talker1_dialog='*',
+                                              talker2_dialog='#')
+
+
+class VocabType(Enum):
+    GPT = 'gpt'
+    GPT2 = 'gpt2'
+
+
+def get_vocab(vocab_path, codes_path, tokenizer_type=VocabType.GPT, zero_shot_special_tokens=False):
+    if tokenizer_type == VocabType.GPT:
+        tokenizer = GPTTokenizer()
+    elif tokenizer_type == VocabType.GPT2:
+        tokenizer = GPT2Tokenizer()
+    else:
+        assert False
+
+    if zero_shot_special_tokens:
+        special_tokens = ZERO_SHOT_SPECIAL_TOKENS
+    else:
+        special_tokens = SPECIAL_TOKENS
+
+    return BPEVocab.from_files(vocab_path, codes_path, tokenizer, special_tokens)
