6b2cbb9631fa44f2eb5cabea1a793698f9618f37
“rauf-kurbanov” <kurbanov.re@gmail.com>
2019-04-11 09:02:30 +0300

    adding make targets

--- platform/entrypoint.sh
+++ platform/entrypoint.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+python convai_router_bot/application.py --port 8080 &
+#python wild.py -bi ${BOT_ID} -rbu ${JOB_URL} &
+/usr/sbin/sshd -D &
+$@
--- platform/inference.sh
+++ platform/inference.sh
@@ -4,9 +4,9 @@
 IMAGE="registry-staging.neu.ro/rauf-kurbanov/transformer_chatbot"
 DATA_ROOT="storage://rauf-kurbanov/transformer_chatbot"
 
-neuro job submit -x --http 8080 --ssh 22 --memory 16G --cpu 4 --gpu 1 \
-                    --gpu-model nvidia-tesla-p4 \
-                    --volume ${DATA_ROOT}/parameters:/workspace/parameterst:ro \
-                    --volume ${DATA_ROOT}/checkpoints:/workspace/checkpoints:ro \
-                    --non-preemptible \
-                    ${IMAGE}
+neuro job submit --http 8080 --ssh 22 --memory 16G --cpu 4 --gpu 1 \
+                 --gpu-model nvidia-tesla-p4 \
+                 --volume ${DATA_ROOT}/parameters:/workspace/parameterst:ro \
+                 --volume ${DATA_ROOT}/checkpoints:/workspace/checkpoints:ro \
+                 --non-preemptible \
+                 ${IMAGE}
