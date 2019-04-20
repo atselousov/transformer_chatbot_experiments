ad0b7619da40c625c5ab38a9a8625535a183c7e4
Sergey Golovanov <Sergey_XG@mail.ru>
2019-04-20 16:56:34 +0300

    update dataset

--- model/dataset.py
+++ model/dataset.py
@@ -16,12 +16,14 @@
 
 import os
 import random
+from collections import defaultdict
 
 import torch
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
-from .postprocessing import augment_replica
+import nltk
+from nltk.corpus import wordnet
 
 
 class FacebookDataset(Dataset):
@@ -104,10 +106,41 @@ class FacebookDataset(Dataset):
             if cache:
                 torch.save(self.data, cache)
         if limit_size > 0:
+            random.suffle(self.data)
             self.data = self.data[:limit_size]
 
     def __len__(self):
         return len(self.data)
+
+    def _augment_replica(seq):
+        _exceptions = ['your', 'persona']
+        pos2wn = {'NN': wordnet.NOUN,
+                'JJ': wordnet.ADJ,
+                'VBP': wordnet.VERB,
+                'RB': wordnet.ADV}
+
+        synonyms = defaultdict(list)
+
+        tagged_seq = seq.replace('i ', 'I ')
+        tagged_seq = nltk.pos_tag(nltk.word_tokenize(tagged_seq))
+
+        for word, pos in tagged_seq:
+            if pos not in pos2wn or word in _exceptions:
+                continue
+
+            pos = pos2wn[pos]
+            synnets = wordnet.synsets(word, pos=pos)
+
+            for synnet in synnets:
+                for syn in synnet.lemma_names():
+                    if syn != word:
+                        synonyms[word].append(syn.replace('_', ' '))
+                break
+        if synonyms:
+            for key, values in synonyms.items():
+                seq = seq.replace(key, random.choice(list(values)))
+
+        return seq
 
     def _augment(self, sentences, info=False):
 
@@ -129,7 +162,7 @@ class FacebookDataset(Dataset):
         def _try2augment(sent):
             if random.uniform(0, 1) < self.aug_syn_proba:
                 sent = self.vocab.ids2string(sent)
-                sent = augment_replica(sent)
+                sent = self._augment_replica(sent)
                 sent = self.vocab.string2ids(sent)
             return sent
 
